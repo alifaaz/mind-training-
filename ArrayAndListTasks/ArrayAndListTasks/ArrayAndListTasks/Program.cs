3a539ed8047cfa1abd65277c2e11b8f7c55a7f94
Mohammed <mohammed.najm.422@gmail.com>
2025-07-22 19:27:50 +0300

    add Exercise 1 Create a list of 5 numbers.

---
+++ ArrayAndListTasks/ArrayAndListTasks/ArrayAndListTasks/Program.cs
@@ -0,0 +1,13 @@
+﻿// كل واحد يسوي كلاس بيه شغله وطبعا على فرع باسمه
+using ArrayAndListTasks;
+
+Tasks task = new Tasks();
+
+int[] numbers = task.ListOfNumbers(5);
+if(numbers.Length > 0)
+{
+    foreach(int number in numbers)
+    {
+        Console.Write(number+", ");
+    }
+}
---
+++ ArrayAndListTasks/ArrayAndListTasks/ArrayAndListTasks/Tasks.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayAndListTasks
+{
+    internal class Tasks
+    {
+        Random rnd = new Random();
+        //------------------------------ Exercise 1 ----------------------------------------------------//
+        // أنشئ قائمة تحتوي على 5 أرقام او حسب الرقم اللي ارسله للداله من استدعيها
+        public int[] ListOfNumbers(int noElement)
+        {
+            try
+            {
+                int[] numbers = new int[noElement];
+                for (int i = 0; i < noElement; i++)
+                {
+                    // اضيف اعداد عشوائيا بالعدد اللي اني نطيته
+                    numbers[i] = rnd.Next(100);
+                }
+                return numbers;
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return null;
+            }            
+        }
+
+
+    }
+}
