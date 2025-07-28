ca884f732a6525679b45eb6b24c856de3c6a6a7c
Mohammed <mohammed.najm.422@gmail.com>
2025-07-28 18:35:08 +0300

    Add oop Project.

---
+++ TheCodeWizards_OOP/MohammedNajmOOP/MohammedNajmOOP/Program.cs
@@ -0,0 +1,17 @@
+﻿
+// Create car objects
+using MohammedNajmOOP;
+
+Car car1 = new Car("Toyota", "Camry", 2020);
+Car car2 = new Car("Honda", "Civic", 2022);
+Car car3 = new Car("Ford", "Mustang", 2021);
+
+// Call methods
+car1.Start();
+car1.Stop();
+
+car2.Start();
+car2.Stop();
+
+car3.Start();
+car3.Stop();
---
+++ TheCodeWizards_OOP/MohammedNajmOOP/MohammedNajmOOP/TaskClasses.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MohammedNajmOOP
+{
+    internal class TaskClasses
+    {
+    }
+    //--------------------------------------------------------------------------------------------------------------
+    //Exercise 1: Create a Car class with attributes brand, model, and year. Add methods start() and stop(). Create 3 car objects and call their methods.
+    class Car
+    {
+        // Properties
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+
+        // Constructor
+        public Car(string brand, string model, int year)
+        {
+            Brand = brand;
+            Model = model;
+            Year = year;
+        }
+
+        // Methods
+        public void Start()
+        {
+            Console.WriteLine($"{Brand} {Model} ({Year}) is starting...");
+        }
+
+        public void Stop()
+        {
+            Console.WriteLine($"{Brand} {Model} ({Year}) is stopping...");
+        }
+    }
+    //--------------------------------------------------------------------------------------------------------------
+    //Exercise 2:
+
+
+}
